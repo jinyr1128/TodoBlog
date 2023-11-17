044c501a8785d906e45054832f7625ba0b0103d2
jinyr1128 <jinyr1128@gmail.com>
2023-11-17 12:07:55 +0900

    Feat:controller

---
+++ src/main/java/yull/todoblog/controller/TodoController.java
@@ -0,0 +1,25 @@
+package yull.todoblog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import yull.todoblog.domain.Article;
+import yull.todoblog.dto.AddArticleRequest;
+import yull.todoblog.service.BlogService;
+
+@RequiredArgsConstructor
+@RestController
+public class TodoController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle (@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
