b18628b1b921fe08a18e5f627bf118c932ec8f13
jinyr1128 <jinyr1128@gmail.com>
2023-11-17 15:25:45 +0900

    Entity Local time add

--- src/main/java/yull/todoblog/controller/BlogViewController.java
+++ src/main/java/yull/todoblog/controller/BlogViewController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import yull.todoblog.domain.Article;
 import yull.todoblog.dto.ArticleListViewResponse;
 import yull.todoblog.service.BlogService;
 
@@ -23,5 +25,12 @@ public class BlogViewController {
         model.addAttribute("articles",articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle (@PathVariable Long id , Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleListViewResponse(article));
+        return "article";
     }
 }
