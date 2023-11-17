9e232885df3a191135bd4b2a0fdc2815b1c4258a
jinyr1128 <jinyr1128@gmail.com>
2023-11-17 11:51:51 +0900

    CRUD구현중 생각나서 얼른 커밋...

---
+++ src/main/java/yull/todoblog/TodoBlogApplication.java
@@ -0,0 +1,13 @@
+package yull.todoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBlogApplication.class, args);
+    }
+
+}
