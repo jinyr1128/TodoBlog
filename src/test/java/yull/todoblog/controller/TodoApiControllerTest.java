dfa6062821437e396b4bbca78ce2b6e2ebb597ac
jinyr1128 <jinyr1128@gmail.com>
2023-11-17 14:11:51 +0900

    Feat : Delete 6577 Delete Test

--- src/test/java/yull/todoblog/controller/TodoApiControllerTest.java
+++ src/test/java/yull/todoblog/controller/TodoApiControllerTest.java
@@ -118,5 +118,26 @@ class TodoApiControllerTest {
                 .andExpect(jsonPath("$.content").value(content))
                 .andExpect(jsonPath("$.title").value(title));
     }
+    @DisplayName("deleteArticle: 블로그 글 삭제에 성공한다.")
+    @Test
+    public void deleteArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
 
+        Article savedArticle = todoRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        mockMvc.perform(delete(url, savedArticle.getId()))
+                .andExpect(status().isOk());
+
+        // then
+        List<Article> articles = todoRepository.findAll();
+
+        assertThat(articles).isEmpty();
+    }
 }
