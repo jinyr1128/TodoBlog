d062e2d32f26aeb67cb1de2ecb1543718cfe05be
jinyr1128 <jinyr1128@gmail.com>
2023-11-20 11:31:13 +0900

    민감정보 가리기

--- src/main/java/yull/todoblog/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package yull.todoblog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yull.todoblog.domain.Article;
-
-public interface TodoRepository extends JpaRepository <Article,Long> {
-}
