8857894f69e865849defd9574d3130a4227aadb0
jinyr1128 <jinyr1128@gmail.com>
2023-12-01 15:41:00 +0900

    UpdateArticleRequestTest

---
+++ src/test/java/yull/todoblog/dto/UpdateArticleRequestTest.java
@@ -0,0 +1,24 @@
+package yull.todoblog.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UpdateArticleRequestTest {
+
+    @Test
+    public void testArticleUpdateRequestFields() {
+        // given: 수정할 제목과 내용으로 객체 생성
+        String expectedTitle = "Updated Title";
+        String expectedContent = "Updated Content";
+        UpdateArticleRequest request = new UpdateArticleRequest(expectedTitle, expectedContent);
+
+        // when: 생성된 객체에서 제목과 내용 검색
+        String actualTitle = request.getTitle();
+        String actualContent = request.getContent();
+
+        // then: 객체에 설정된 제목과 내용이 정상적으로 검색되는지 검증
+        assertEquals(expectedTitle, actualTitle);
+        assertEquals(expectedContent, actualContent);
+    }
+}
