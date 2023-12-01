690fa4d6e7b22406455e733d3312b573cfea5936
jinyr1128 <jinyr1128@gmail.com>
2023-12-01 15:31:24 +0900

    LoginRequestTest

---
+++ src/test/java/yull/todoblog/dto/LoginRequestTest.java
@@ -0,0 +1,26 @@
+package yull.todoblog.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginRequestTest {
+
+    @Test
+    public void testUsernameAndPasswordHandling() {
+        // given: 사용자 이름과 비밀번호 설정
+        String expectedUsername = "testUser";
+        String expectedPassword = "testPassword";
+        LoginRequest request = new LoginRequest();
+        request.setUsername(expectedUsername);
+        request.setPassword(expectedPassword);
+
+        // when: 설정된 사용자 이름과 비밀번호 검색
+        String actualUsername = request.getUsername();
+        String actualPassword = request.getPassword();
+
+        // then: 설정된 사용자 이름과 비밀번호가 정상적으로 검색되는지 검증
+        assertEquals(expectedUsername, actualUsername);
+        assertEquals(expectedPassword, actualPassword);
+    }
+}
