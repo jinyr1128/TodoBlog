3d751039e2488190f2ca340f74a20e434114a371
jinyr1128 <jinyr1128@gmail.com>
2023-12-04 00:30:52 +0900

    jacoco

--- src/main/java/yull/todoblog/TodoBlogApplication.java
+++ src/main/java/yull/todoblog/TodoBlogApplication.java
@@ -3,14 +3,10 @@ package yull.todoblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-
-@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaAuditing
 public class TodoBlogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TodoBlogApplication.class, args);
     }
-
 }
